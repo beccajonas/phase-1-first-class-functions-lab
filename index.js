2b5ad2a6f96560d495fe37088696ee7a5e778a7a
Becca Jonas <beccajonas3@gmail.com>
2023-10-31 13:08:39 -0400

    first class functions lab solutions

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    return drivers.splice(0,2);
+  }
+
+const returnLastTwoDrivers = (drivers) => {
+    drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    return drivers.splice(2,4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+   return function (fare) {
+    return fare * integer;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, selectDrivers) {
+    drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+    if (selectDrivers === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers();
+    } else if (selectDrivers === returnLastTwoDrivers) {
+        return returnLastTwoDrivers();
+    } 
+}
+
+
+returnFirstTwoDrivers();
+returnLastTwoDrivers();
+createFareMultiplier();
+
+
+
+
